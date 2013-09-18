5544d54aba94a2b8c77de3910ff9ce85f2adc052
colab_01.diem <runtim3.error@gmail.com>
2013-09-18 09:21:35 -0600

    ...

--- entrada.php
+++ entrada.php
@@ -17,7 +17,7 @@
 	if (!isset($_TEMA_APP) ) $_TEMA_APP='default';
 	if (!isset($_DEFAUL_LAYOUT) ) $_DEFAUL_LAYOUT='layout';
 	
-	if (!isset($_DEFAULT_APP) ) $_DEFAULT_APP='portal';	
+	if (!isset($_DEFAULT_APP) ) $_DEFAULT_APP='portal';		
 	if (!isset($_DEFAULT_CONTROLLER) ) $_DEFAULT_CONTROLLER='paginas';
 	if (!isset($_DEFAULT_ACTION) ) $_DEFAULT_ACTION='inicio';		
 	if (!isset($APPS_PATH) ) $APPS_PATH='../';	 //ruta relativa de la aplicacion
